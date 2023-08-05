927ecac52d767c6135169633c7333d15580fc9cd
Iamneo <techadmin@iamneo.ai>
2023-08-05 14:08:00 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/TaskRepo.java
+++ springapp/src/main/java/com/examly/springapp/TaskRepo.java
@@ -1,3 +1,22 @@
-public class TaskRepo {
-    
+package com.examly.springapp;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+
+
+
+public interface TaskRepo extends JpaRepository<Task,Integer>{
+	
+	Optional<Task> findBytaskHolderNameLike(String name);
+	
+	@Modifying
+	@Transactional
+	@Query(value="update Task set task_status = 'Completed' where task_id = ?1",nativeQuery=true	)
+	void updateTaskStatus(int id);
+
 }
